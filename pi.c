1d462b6331f1ce73087126426e93f48574c50bdd
Graeme Winter <graeme.winter@gmail.com>
2022-04-17 21:30:23 +0100

    Basic code

---
+++ pi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+
+int main()
+{
+  stdio_init_all();
+
+  for (;;) {
+    int a[52514], b = 0, c = 52514, d = 0, e = 0, f = 1e4, g = 0, h = 0;
+
+    for (; (b = c -= 14); (h = printf("%04d", e + d / f)))
+      for ((e = d %= f); (g = --b * 2); (d /= g))
+        d = d * b + f * (h ? a[b] : f / 5), a[b] = d % --g;
+
+    printf("\n\n\n");
+  }
+
+  return 0;
+}
+
