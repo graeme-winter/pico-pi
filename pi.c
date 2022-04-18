ec9b9db3a1d5d76cc9e533f838b7146ad445f495
Graeme Winter <graeme.winter@gmail.com>
2022-04-18 21:28:48 +0100

    Timing

--- pi.c
+++ pi.c
@@ -5,13 +5,15 @@ int main() {
   stdio_init_all();
 
   for (;;) {
+    absolute_time_t t0 = get_absolute_time();
     int a[52514], b = 0, c = 52514, d = 0, e = 0, f = 1e4, g = 0, h = 0;
 
     for (; (b = c -= 14); (h = printf("%04d", e + d / f)))
       for ((e = d %= f); (g = --b * 2); (d /= g))
         d = d * b + f * (h ? a[b] : f / 5), a[b] = d % --g;
 
-    printf("\n\n\n");
+    absolute_time_t t1 = get_absolute_time();
+    printf("\n%ld µs\n", absolute_time_diff_us(t0, t1));
   }
 
   return 0;
