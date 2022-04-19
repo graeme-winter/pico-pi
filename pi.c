b2b4ee3f102174a68567e54810270c88252a44c0
Graeme Winter <graeme.winter@gmail.com>
2022-04-19 06:41:43 +0100

    Seconds

--- pi.c
+++ pi.c
@@ -5,15 +5,16 @@ int main() {
   stdio_init_all();
 
   for (;;) {
-    absolute_time_t t0 = get_absolute_time();
+    double t0, t1;
+    t0 = 1e-6 * to_us_since_boot(get_absolute_time());
     int a[52514], b = 0, c = 52514, d = 0, e = 0, f = 1e4, g = 0, h = 0;
 
     for (; (b = c -= 14); (h = printf("%04d", e + d / f)))
       for ((e = d %= f); (g = --b * 2); (d /= g))
         d = d * b + f * (h ? a[b] : f / 5), a[b] = d % --g;
 
-    absolute_time_t t1 = get_absolute_time();
-    printf("\n%ld microseconds\n", absolute_time_diff_us(t0, t1));
+    t1 = 1e-6 * to_us_since_boot(get_absolute_time());
+    printf("\n%f s\n", t1 - t0);
   }
 
   return 0;
